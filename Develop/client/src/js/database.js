b03ce40e0ab8868ebc18cdb2618080ea7ebb753d
Aubrey Gunn <agunn369@gmail.com>
2023-06-21 22:01:19 -0500

    added logic for database and install

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const editorDb = await openDB('jate', 1);
+  const transaction = editorDb.transaction('jate', 'readwrite');
+  const openStore = transaction.objectStore('jate');
+  const request = openStore.put({ id: 1, value: content });
+  const result = await request;
+  console.error('putDb not implemented');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const editorDb = await openDB('jate', 1);
+  const transaction = editorDb.transaction('jate', 'readonly');
+  const openStore = transaction.objectStore('jate');
+  const request = openStore.get(1);
+  const result = await request;
+  result
+    ? console.log('data found, result.value')
+    : console.log('data not found');
+
+  return result?.value;
+};
 
 initdb();
